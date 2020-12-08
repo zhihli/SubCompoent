f540dd258b84462bcd9962daacb2d317900502eb
zhihli <zhli@voxelcloud.net.cn>
2020-12-08 14:40:09 +0800

    add button size

--- CustomButton/index.js
+++ CustomButton/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
-
-export default () => {
-    return <button>按钮</button>
+const styles = {
+    large: { height: '48px'},
+    small: { height: '16px'},
+    default: { height: '32px'}
 }
+const Button = ({ size }) => {
+    return <button style={styles[size|| 'default']}>按钮</button>
+}
+
+export default Button
